28ff0437ff17131330ecfc32de0e7fb81c619381
mac craig <maccraig98@gmail.com>
2017-11-24 11:36:27 -0800

    Use mysqli

--- testing.php
+++ testing.php
@@ -5,12 +5,12 @@ $dbname = 'tiny';
 $dbuser = 'tinyUser';
 $dbpass = getenv('PW');
 $dbhost = 'localhost';
-$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-mysql_select_db($dbname) or die("Could not open the db '$dbname'");
+$connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($dbname) or die("Could not open the db '$dbname'");
 $test_query = "SHOW TABLES FROM $dbname";
-$result = mysql_query($test_query);
+$result = mysqli_query($test_query);
 $tblCnt = 0;
-while($tbl = mysql_fetch_array($result)) {
+while($tbl = mysqli_fetch_array($result)) {
   $tblCnt++;
   #echo $tbl[0]."<br />\n";
 }
